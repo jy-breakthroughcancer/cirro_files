7cb229dfa448f6f1940b0c8fad33602dcfbb6aff
jy-breakthroughcancer <142935283+jy-breakthroughcancer@users.noreply.github.com>
2023-09-12 19:44:10 -0400

    Update preprocess.py

--- gatk_germline_cnv/preprocess.py
+++ gatk_germline_cnv/preprocess.py
@@ -3,5 +3,5 @@
 import json
 from cirro.helpers.preprocess_dataset import PreprocessDataset
 
-s = PreprocessDataset.from_running()
+ds = PreprocessDataset.from_running()
 json.dump(ds.params, open('inputs.0.json', 'w'))
