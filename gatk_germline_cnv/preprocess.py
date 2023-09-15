e83bc80c0742a28435e3338ea4b3b3aeb8d2e53b
Jen Yao <58090792+jiangyao08@users.noreply.github.com>
2023-09-15 11:25:17 -0400

    1. change the values for some parameters. 2. change logger to log in preprocess.py

--- gatk_germline_cnv/preprocess.py
+++ gatk_germline_cnv/preprocess.py
@@ -34,8 +34,8 @@ def setup_inputs(ds: PreprocessDataset):
                 for kw, val in ds.params.items()
                 if kw.startswith("CNVGermlineCohortWorkflow")
             }
-    ds.logger("Display all_inputs")
-    ds.logger(all_inputs)
+    ds.log("Display all_inputs")
+    ds.log(all_inputs)
 
     # Write out the complete set of inputs
     write_json("inputs.json", all_inputs)
